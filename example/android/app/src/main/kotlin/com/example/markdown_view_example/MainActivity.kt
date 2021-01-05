cfbfb7f397ddc8a2e01516066159534134e0d7a4
haoyi <39499740@qq.com>
2021-01-05 16:04:50 +0800

    添加测试字符串

---
+++ example/android/app/src/main/kotlin/com/example/markdown_view_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.markdown_view_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/markdown_view.dart
+++ lib/markdown_view.dart
@@ -1,18 +1,43 @@
+/*
+ * @Date: 2021-01-04 17:34:16
+ * @LastEditors: 郝怿
+ * @LastEditTime: 2021-01-05 16:04:21
+ * @FilePath: /markdown_view/lib/markdown_view.dart
+ */
 // You have generated a new plugin project without
 // specifying the `--platforms` flag. A plugin project supports no platforms is generated.
 // To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
-import 'dart:async';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/services.dart';
+class MarkdownView extends StatefulWidget {
+  final String markdownSource;
+  MarkdownView({@required this.markdownSource, Key key}) : super(key: key);
+  @override
+  _MarkdownViewState createState() => _MarkdownViewState();
+}
 
-class MarkdownView {
-  static const MethodChannel _channel =
-      const MethodChannel('markdown_view');
+class _MarkdownViewState extends State<MarkdownView> {
+  void _getMarkdownWidgetTree() {
+    List<String> markdownTree =
+        widget.markdownSource.replaceAll("  \n", "\n\n").split("\n\n");
+    for (int i = 0; i < markdownTree.length; i++) {
+      print(markdownTree[i]);
+    }
+  }
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            _getMarkdownWidgetTree();
+          },
+          child: Text("解析"),
+        ),
+      ),
+    );
   }
 }
